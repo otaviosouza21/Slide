f0ca69e01b2c5782f347f1e738831bb7f2439ef5
Otavio Souza <otaviosouzalu@gmail.com>
2023-04-11 23:18:08 -0300

    adicionado animacao prev nav

--- js/slide.js
+++ js/slide.js
@@ -9,6 +9,10 @@ export default class Slide {
     };
   }
 
+  transition(active){
+    this.slide.style.transition = active ? 'transform .3s' : ''
+  }
+
   moveSlide(distX) {
     this.dist.movePosition = distX;
     this.slide.style.transform = `translate3d(${distX}px,0,0)`;
@@ -30,6 +34,7 @@ export default class Slide {
       moveType = 'touchmove';
     }
     this.wrapper.addEventListener(moveType, this.onMove);
+    this.transition(false)
   }
 
   onMove(event) {
@@ -46,7 +51,21 @@ export default class Slide {
     const moveType = event.type === 'mouseup' ? 'mousemove' : 'touchmove';
     this.wrapper.removeEventListener(moveType, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
+    this.transition(true)
+    this.changeSlideonEnd()
+    this.transition(true)
   }
+
+changeSlideonEnd(){
+  if(this.dist.movement > 120 && this.index.next != undefined){
+    this.activeNextSlide()
+  }
+  else if( this.dist.movement < -120 && this.index.prev != undefined){
+    this.activePrevSlide();
+  } else{
+    this.changeSlide(this.index.active)
+  }
+}
 
   addSlideEvents() {
     this.wrapper.addEventListener('mousedown', this.onStart);
@@ -75,30 +94,39 @@ export default class Slide {
     });
   }
 
-slidesIndexNav(index){
-  const last = this.slideArray.length - 1;
-  this.index = {
-    prev: index ? index - 1 : undefined,
-    active: index,
-    next: index === last ? undefined : index + 1
+  slidesIndexNav(index) {
+    const last = this.slideArray.length - 1;
+    this.index = {
+      prev: index ? index - 1 : undefined,
+      active: index,
+      next: index === last ? undefined : index + 1,
+    };
+    console.log(this.index);
   }
-  console.log(this.index)
-}
 
+  changeSlide(index) {
+    const activeSlice = this.slideArray[index];
+    this.moveSlide(activeSlice.position);
+    this.slidesIndexNav(index);
+    this.dist.finalPosition = activeSlice.position;
+  }
 
-changeSlide(index){
-  const activeSlice = this.slideArray[index];
-  this.moveSlide(activeSlice.position);
-  this.slidesIndexNav(index);
-  this.dist.finalPosition = activeSlice.position
-}
+  //prev Next
+  activePrevSlide() {
+    if (this.index.prev !== undefined) this.changeSlide(this.index.prev);
+  }
+
+  activeNextSlide() {
+    if (this.index.next !== undefined) this.changeSlide(this.index.next);
+  }
+
 
 
   init() {
     this.bindEvents();
     this.addSlideEvents();
     this.slidesConfig();
+    this.transition(true)
     return this;
   }
 }
-
